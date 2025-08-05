953948e6c40655f395dea48c23c2dc2696414d3a
Lihan Zha <lihanzha20@gmail.com>
2025-08-05 19:59:53 -0400

    fix checkpoint_dir (3)

--- src/openpi/training/checkpoints.py
+++ src/openpi/training/checkpoints.py
@@ -20,6 +20,8 @@ import openpi.training.utils as training_utils
 def initialize_checkpoint_dir(
     checkpoint_dir: epath.Path | str, *, keep_period: int | None, overwrite: bool, resume: bool
 ) -> tuple[ocp.CheckpointManager, bool]:
+    logging.info(f"Checkpoint_dir:{checkpoint_dir}")
+
     checkpoint_dir = epath.Path(checkpoint_dir).resolve()
     logging.info(f"Checkpoint_dir:{checkpoint_dir}")
     resuming = False
