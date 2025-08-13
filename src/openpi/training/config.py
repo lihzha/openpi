da561fe8e21aed4f8cba13a092c617ecdabbe58f
Lihan Zha <lihanzha20@gmail.com>
2025-08-13 13:35:14 -0400

    set fsdp=1 and batch size=16

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -652,7 +652,7 @@ _CONFIGS = [
         ),
         num_train_steps=100_000,
         fsdp_devices=4,
-        batch_size=1,
+        batch_size=16,
         weight_loader=weight_loaders.PaliGemmaWeightLoader(),
         assets_base_dir="gs://pi0-cot/assets",
         checkpoint_base_dir="gs://pi0-cot/checkpoints",
