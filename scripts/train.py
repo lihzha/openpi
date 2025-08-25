b7def6e3cafecaacf4a7bad1ce7950e891c3e506
Lihan Zha <lihanzha20@gmail.com>
2025-08-24 21:26:57 -0400

    fix test policy infer tpu (16)

--- scripts/train.py
+++ scripts/train.py
@@ -575,9 +575,9 @@ def main(config: _config.TrainConfig):
     for step in pbar:
         with sharding.set_mesh(mesh):
             train_state, info = ptrain_step(train_rng, train_state, batch)
+        infos.append(info)
         stacked_infos = common_utils.stack_forest(infos)
         reduced_info = jax.device_get(jax.tree.map(jnp.mean, stacked_infos))
-        infos.append(info)
         info_str = ", ".join(f"{k}={v:.4f}" for k, v in reduced_info.items())
         logging.info(f"Step {step}: {info_str}")
 
