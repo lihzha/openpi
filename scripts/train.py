8654a139f518aec9412e3f99ab092fddfac72ead
Lihan Zha <lihanzha20@gmail.com>
2025-08-27 10:31:42 -0400

    update v4 (6)

--- scripts/train.py
+++ scripts/train.py
@@ -433,6 +433,7 @@ def eval_step(
     observation = batch[0]
     logits, t, _, _, _ = model.sample_reasoning(observation)
     logging.info(f"Logits: {logits.shape}, t: {jax.device_get(t)}")
+    breakpoint()
     tokenized_reasoning = tok.decode(jax.device_get(logits).squeeze()[:jax.device_get(t)])
     return tokenized_reasoning
 
